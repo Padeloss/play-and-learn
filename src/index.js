0fc8f6ee6a0e4a675f3e99d67ce8a814dee11b3c
Padeloss <pantelisxa@hotmail.com>
2024-11-13 09:35:03 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-
-// index.js (Backend)
-
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const app = express();
-
-// Middleware
-app.use(bodyParser.json());
-
-// Database connection
-mongoose.connect('mongodb://localhost:27017/playandlearn', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Routes
-app.get('/', (req, res) => {
-  res.send('Hello, Play and Learn!');
-});
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
