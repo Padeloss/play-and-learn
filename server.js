1c2d6f58401663208053603cf5d813f129df70b7
Padeloss <124687366+Padeloss@users.noreply.github.com>
2025-02-27 08:02:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
-const app = require('./backend/index');
+const express = require('express');
+const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = 3000;
+
+// Σερβίρουμε τα static αρχεία από τον φάκελο "public"
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
